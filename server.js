caa25670429513a6d96fdc8f0c1c101c079e9304
CJ Ellis <cellis@talentpath.com>
2019-12-03 16:16:26 -0600

    fixed server

--- server.js
+++ server.js
@@ -1,11 +1,62 @@
 var http = require("http");
 //var https = require("https"); ERR_SSL_VERSION_OR_CIPHER_MISMATCH
-//var XLSX = require("xlsx");
-//var cors = require("cors");
+var XLSX = require("xlsx");
+var cors = require("cors");
 
 var hostname = "localhost";
 var port = process.env.PORT || 4243;
-//var formattedEverything = {};
+// var formattedEverything = {};
+
+// Set up workbook for server start
+// Get excel document flood-data.xlsx
+getWorksheet = () => {
+  workbook = XLSX.readFile("./flood-data.xlsx");
+  first_sheet = workbook.SheetNames[1];
+  return workbook.Sheets[first_sheet];
+};
+var worksheet = getWorksheet();
+
+getEverything = worksheet => {
+  var addressArray = {};
+  for (i = 2; i > 0; i++) {
+    var idCellName = "A" + i;
+    var addressCellName = "H" + i;
+    var insideDMGCellName = "K" + i;
+    var parkingDMGCellName = "L" + i;
+    var idCell = worksheet[idCellName];
+    var addressCell = worksheet[addressCellName];
+    var insideDMGCell = worksheet[insideDMGCellName];
+    var parkingDMGCell = worksheet[parkingDMGCellName];
+    var idValue = idCell ? idCell.v : undefined;
+    var addressValue = addressCell ? addressCell.v : undefined;
+    var insideDMGValue = insideDMGCell ? insideDMGCell.v : undefined;
+    var parkingDMGValue = parkingDMGCell ? parkingDMGCell.v : undefined;
+    if (idValue === undefined) {
+      break;
+    }
+    if (insideDMGValue === "Yes") {
+      insideDMGValue = 1;
+    } else if (insideDMGValue === "No") {
+      insideDMGValue = -1;
+    } else {
+      insideDMGValue = 0;
+    }
+    if (parkingDMGValue === "Yes") {
+      parkingDMGValue = 1;
+    } else if (parkingDMGValue === "No") {
+      parkingDMGValue = -1;
+    } else {
+      parkingDMGValue = 0;
+    }
+
+    addressArray[idValue] = {
+      addressValue,
+      insideDMGValue,
+      parkingDMGValue
+    };
+  }
+  return JSON.stringify(addressArray);
+};
 
 setInterval(() => {
   worksheet = getWorksheet();
@@ -22,8 +73,8 @@ var server = http.createServer((request, response) => {
     response.end();
   }
   // if (request.method === "GET" && request.url === "/map/formatted") {
-  //   response.write(JSON.stringify(formattedEverything));
-  //   //fetch;
+  //   // response.write(JSON.stringify(formattedEverything));
+  //   fetch;
   //   response.end();
   // }
   // if (request.method === "POST" && request.url === "/map/formatted") {
