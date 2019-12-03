e778e0d1b560dd5c55b40d2ca2d903cb8014ad9c
CJ Ellis <cellis@talentpath.com>
2019-12-03 12:38:31 -0600

    changed port

--- server.js
+++ server.js
@@ -65,38 +65,38 @@ setInterval(() => {
 var server = https.createServer((request, response) => {
   response.setHeader("Access-Control-Allow-Origin", "*");
   if (request.method === "GET" && request.url === "/") {
-    response.write("nice yeet");
-    response.end();
-  }
-  if (request.method === "GET" && request.url === "/map/everything") {
-    response.write(getEverything(worksheet));
-    response.end();
-  }
-  if (request.method === "GET" && request.url === "/map/formatted") {
     response.write(JSON.stringify(formattedEverything));
     response.end();
   }
-  if (request.method === "POST" && request.url === "/map/formatted") {
-    request.on("data", function(chunk) {
-      // console.log("Received body data:");
-      // console.log(chunk.toString());
-      // console.log(JSON.parse(chunk.toString()).id);
-      for (id in formattedEverything) {
-        if (
-          formattedEverything[id].formattedHeld ===
-          JSON.parse(chunk.toString()).formattedHeld
-        ) {
-          formattedEverything[id].totalResidents = JSON.parse(
-            chunk.toString()
-          ).totalResidents;
-        }
-      }
-      formattedEverything[JSON.parse(chunk.toString()).id] = JSON.parse(
-        chunk.toString()
-      );
-    });
-    response.end();
-  }
+  // if (request.method === "GET" && request.url === "/map/everything") {
+  //   response.write(getEverything(worksheet));
+  //   response.end();
+  // }
+  // if (request.method === "GET" && request.url === "/map/formatted") {
+  //   response.write(JSON.stringify(formattedEverything));
+  //   response.end();
+  // }
+  // if (request.method === "POST" && request.url === "/map/formatted") {
+  //   request.on("data", function(chunk) {
+  //     // console.log("Received body data:");
+  //     // console.log(chunk.toString());
+  //     // console.log(JSON.parse(chunk.toString()).id);
+  //     for (id in formattedEverything) {
+  //       if (
+  //         formattedEverything[id].formattedHeld ===
+  //         JSON.parse(chunk.toString()).formattedHeld
+  //       ) {
+  //         formattedEverything[id].totalResidents = JSON.parse(
+  //           chunk.toString()
+  //         ).totalResidents;
+  //       }
+  //     }
+  //     formattedEverything[JSON.parse(chunk.toString()).id] = JSON.parse(
+  //       chunk.toString()
+  //     );
+  //   });
+  //   response.end();
+  // }
 });
 
 server.listen(port, () => {
