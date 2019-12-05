91f4400c0f8cd8f254a5da921b59da233150e59f
CJ Ellis <cellis@talentpath.com>
2019-12-04 20:05:10 -0600

    made gitpush perfect

--- gitpush.sh
+++ gitpush.sh
@@ -1,11 +1,5 @@
-<<<<<<< HEAD
 rclone sync -v floodmap:Documents /home/pi/Documents
 git add .
 git commit -m "new data"
 git push origin master
 rclone sync -v /home/pi/Documents floodmap:Documents
-=======
-git add .
-git commit -m "new data"
-git push origin master
->>>>>>> 63c766be230afd0185617f0cec6bf109e6653f93
